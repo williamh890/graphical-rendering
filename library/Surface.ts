ed739f717e53f0da2d9948b2ab0647ec9d21f0a8
William Horn <wbhorn@alaska.edu>
2018-02-09 15:57:30 -0900

    Only necessary hw1 files.

--- library/Surface.ts
+++
@@ -1,9 +0,0 @@
-class Surface {
-    public count: number = 0;
-    constructor(readonly mode: number, readonly offset: number,
-        readonly mtllib: string, readonly mtl: string) {
-    }
-    Add(): void {
-        this.count++;
-    }
-}
